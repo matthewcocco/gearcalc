59379504a11b0068339e3ea4d991182b72f3f248
Matthew Cocco <matthew.cocco@gmail.com>
2013-03-19 15:08:45 -0500

    cleaned readme, made gearcalc.py more flexible

--- README.md
+++ README.md
@@ -3,42 +3,72 @@ gearcalc
 
 Written as part of a class project.
 
-**Objective: design a two-speed transmission**.
+**Objective: design a simple two-speed transmission**. *(by picking 4 gears)*
 + Pick 4 gears from the [McMaster-Carr website](http://www.mcmaster.com/#standard-gears/)
 + Gears should provide transmission ratios of 0.5 and 1.5 between the two axles
 + Centers of axles should be 1" apart
-+ Cost <= $15 *(I am not sure this condition is possible)*
++ Cost <= $15 *(impossible?)*
+	- *("[Molded Nylon Plain Bore](http://www.mcmaster.com/#standard-gears/=ly6yze)" gears appear to be the cheapest.)*
+
 
 This is a short script in python to:
 + read data about a gears from an INI file
 + find sets of gears that fit constraints
 
-------------
+---------------------------------------
 
 results
 -------
-Combination found: 
-	Total cost = $25.07
-		Ratio of 1 to 3 = 0.444444444444
-			(32.0 : 72.0)
-		Ratio of 2 to 4 = 1.6
-			(64.0 : 40.0)
-		Axle-distance   = 1.12 inches
-	Part Numbers:
-		Gear 1 :"57655K18"
-		Gear 2: "57655K27"
-		Gear 3: "57655K28"
-		Gear 4: "57655K21"
+Combination found:
 
-Combination found: 
-	Total cost = $24.84
-		Ratio of 1 to 3 = 0.5625
-			(36.0 : 64.0)
-		Ratio of 2 to 4 = 1.5
-			(60.0 : 40.0)
-		Axle-distance   = 1.08 inches
-	Part Numbers:
-		Gear 1 :"57655K19"
-		Gear 2: "57655K26"
-		Gear 3: "57655K27"
-		Gear 4: "57655K21"
+    Total cost = $25.07
+    	Ratio of 1 to 3 = 0.444444444444
+    		(32.0 : 72.0)
+    	Ratio of 2 to 4 = 1.6
+    		(64.0 : 40.0)
+    	Axle-distance   = 1.12 inches
+    Part Numbers:
+    	Gear 1 :"57655K18"
+    	Gear 2: "57655K27"
+    	Gear 3: "57655K28"
+    	Gear 4: "57655K21"
+
+Combination found:
+
+    Total cost = $24.84
+    	Ratio of 1 to 3 = 0.5625
+    		(36.0 : 64.0)
+    	Ratio of 2 to 4 = 1.5
+    		(60.0 : 40.0)
+    	Axle-distance   = 1.08 inches
+    Part Numbers:
+    	Gear 1 :"57655K19"
+    	Gear 2: "57655K26"
+    	Gear 3: "57655K27"
+    	Gear 4: "57655K21"
+
+---------------------------------------
+
+values used:
+------------
+The gears.ini file was built by hand from the values on McMaster-Carr's [Molded Nylon Plain Bore](http://www.mcmaster.com/#standard-gears/=ly6yze) page.
+converted them using [a table I found](http://www.seoconsultants.com/charts/inches-decimal/)
+
+    # 12, 0.250, 3/16, 0.29, 5/16, 1/8, 3/32, "57655K11", 6.15
+    # 14, 0.292, 1/4,  0.33, 5/16, 1/8, 3/32, "57655K12", 4.38
+    # 16, 0.333, 1/4,  0.37, 5/16, 1/8, 1/8,  "57655K13", 4.66
+    # 18, 0.375, 5/16, 0.41, 5/16, 1/8, 1/8,  "57655K14", 4.60
+    # 20, 0.417, 3/8,  0.45, 3/8,  1/8, 1/8,  "57655K15", 4.09
+    # 24, 0.500, 3/8,  0.54, 3/8,  1/8, 1/8,  "57655K16", 4.84
+    # 28, 0.583, 1/2,  0.62, 3/8,  1/8, 1/8,  "57655K17", 4.93
+    # 32, 0.667, 1/2,  0.70, 3/8,  1/8, 1/8,  "57655K18", 5.06
+    # 36, 0.750, 5/8,  0.79, 3/8,  1/8, 3/16, "57655K19", 5.00
+    # 40, 0.833, 5/8,  0.87, 3/8,  1/8, 3/16, "57655K21", 5.90
+    # 42, 0.875, 7/16, 0.91, 3/8,  1/8, 1/4,  "57655K22", 4.12
+    # 48, 1.000, 5/8,  1.04, 3/8,  1/8, 3/16, "57655K24", 5.69
+    # 56, 1.167, 5/8,  1.20, 3/8,  1/8, 3/16, "57655K25", 6.64
+    # 60, 1.250, 5/8,  1.29, 3/8,  1/8, 1/4,  "57655K26", 6.89
+    # 64, 1.333, 5/8,  1.37, 3/8,  1/8, 1/4,  "57655K27", 7.05
+    # 72, 1.500, 5/8,  1.54, 3/8,  1/8, 1/4,  "57655K28", 7.06
+    # 80, 1.667, 5/8,  1.70, 3/8,  1/8, 1/4,  "57655K29", 7.28
+    # 96, 2.000, 1,    2.04, 3/8,  1/8, 1/4,  "57655K31", 6.16
--- gearcalc.py
+++ gearcalc.py
@@ -2,73 +2,73 @@
 
 import ConfigParser
 
+# config values
+
 gearDataFile = "gears.ini"
 
-# converted values by hand using http://www.seoconsultants.com/charts/inches-decimal/ 
-# list taken from mcmaster.com
+ideal_ratio_1_3 = 0.5
+tolerance_1_3   = 0.1
 
-# 12, 0.250, 3/16, 0.29, 5/16, 1/8, 3/32, "57655K11", 6.15
-# 14, 0.292, 1/4,  0.33, 5/16, 1/8, 3/32, "57655K12", 4.38
-# 16, 0.333, 1/4,  0.37, 5/16, 1/8, 1/8,  "57655K13", 4.66
-# 18, 0.375, 5/16, 0.41, 5/16, 1/8, 1/8,  "57655K14", 4.60
-# 20, 0.417, 3/8,  0.45, 3/8,  1/8, 1/8,  "57655K15", 4.09
-# 24, 0.500, 3/8,  0.54, 3/8,  1/8, 1/8,  "57655K16", 4.84
-# 28, 0.583, 1/2,  0.62, 3/8,  1/8, 1/8,  "57655K17", 4.93
-# 32, 0.667, 1/2,  0.70, 3/8,  1/8, 1/8,  "57655K18", 5.06
-# 36, 0.750, 5/8,  0.79, 3/8,  1/8, 3/16, "57655K19", 5.00
-# 40, 0.833, 5/8,  0.87, 3/8,  1/8, 3/16, "57655K21", 5.90
-# 42, 0.875, 7/16, 0.91, 3/8,  1/8, 1/4,  "57655K22", 4.12
-# 48, 1.000, 5/8,  1.04, 3/8,  1/8, 3/16, "57655K24", 5.69
-# 56, 1.167, 5/8,  1.20, 3/8,  1/8, 3/16, "57655K25", 6.64
-# 60, 1.250, 5/8,  1.29, 3/8,  1/8, 1/4,  "57655K26", 6.89
-# 64, 1.333, 5/8,  1.37, 3/8,  1/8, 1/4,  "57655K27", 7.05
-# 72, 1.500, 5/8,  1.54, 3/8,  1/8, 1/4,  "57655K28", 7.06
-# 80, 1.667, 5/8,  1.70, 3/8,  1/8, 1/4,  "57655K29", 7.28
-# 96, 2.000, 1,      2.04, 3/8,  1/8, 1/4,  "57655K31", 6.16
+ideal_ratio_2_4 = 1.5
+tolerance_2_4   = 0.1
+
+min_distance = 1.0 # in inches
+max_distance = 2.0 # also in inches
+
+# a few calculations before we begin
+
+minr_1_3 = ideal_ratio_1_3 - tolerance_1_3
+maxr_1_3 = ideal_ratio_1_3 + tolerance_1_3
+
+minr_2_4 = ideal_ratio_2_4 - tolerance_2_4
+maxr_2_4 = ideal_ratio_2_4 + tolerance_2_4
+
+# open file to parse
+
+cfp = ConfigParser.ConfigParser()
+cfp.readfp(open(gearDataFile))
 
 class Gear:
-	def __init__(self, gearNumber): 
-		cfp = ConfigParser.ConfigParser()
-		cfp.readfp(open(gearDataFile))
-		gearname = 'Gear' + str(gearNumber)
-		self.teeth              = float(cfp.get(gearname, 'Teeth'))
-		self.pitchDiameter      = float(cfp.get(gearname, 'PitchDiameter'))
-		self.hubDiameter        = float(cfp.get(gearname, 'HubDiameter'))
-		self.orthogonalDiameter = float(cfp.get(gearname, 'OrthogonalDiameter'))
-		self.overallLength      = float(cfp.get(gearname, 'OverallLength'))
-		self.faceWidth          = float(cfp.get(gearname, 'FaceWidth'))
-		self.boreSize           = float(cfp.get(gearname, 'BoreSize'))
-		self.partNumber         = cfp.get(gearname, 'PartNumber')
-		self.price              = float(cfp.get(gearname, 'Price'))
+    def __init__(self, gearNumber): 
+        gearname = 'Gear' + str(gearNumber)
+        self.teeth              = float(cfp.get(gearname, 'Teeth'))
+        self.pitchDiameter      = float(cfp.get(gearname, 'PitchDiameter'))
+        self.hubDiameter        = float(cfp.get(gearname, 'HubDiameter'))
+        self.orthogonalDiameter = float(cfp.get(gearname, 'OrthogonalDiameter'))
+        self.overallLength      = float(cfp.get(gearname, 'OverallLength'))
+        self.faceWidth          = float(cfp.get(gearname, 'FaceWidth'))
+        self.boreSize           = float(cfp.get(gearname, 'BoreSize'))
+        self.partNumber         = cfp.get(gearname, 'PartNumber')
+        self.price              = float(cfp.get(gearname, 'Price'))
 
-gears = [ Gear(i) for i in range(1,19) ]
+gears = [ Gear(i) for i in range(1,19) ] # make a list, gears, of objects starting at Gear(1) and ending at Gear(18)
 
 for gear1 in gears:
-	for gear2 in gears:
-		for gear3 in gears:
-			for gear4 in gears:
-				length_1_3 = (gear1.orthogonalDiameter / 2 + gear3.orthogonalDiameter / 2)
-				length_2_4 = (gear2.orthogonalDiameter / 2 + gear4.orthogonalDiameter / 2)
+    for gear2 in gears:
+        for gear3 in gears:
+            for gear4 in gears:
+                dist_1_3 = (gear1.orthogonalDiameter / 2 + gear3.orthogonalDiameter / 2)
+                dist_2_4 = (gear2.orthogonalDiameter / 2 + gear4.orthogonalDiameter / 2)
 
-				correctLength = (length_1_3 == length_2_4 and length_1_3 >= 1.0 and length_2_4 <= 2.0)
+                correctDistance = (dist_1_3 == dist_2_4 and dist_1_3 >= min_distance and dist_1_3 <= max_distance)
 
-				ratio_1_3 = (gear1.teeth / gear3.teeth)
-				ratio_2_4 = (gear2.teeth / gear4.teeth)
+                ratio_1_3 = (gear1.teeth / gear3.teeth)
+                ratio_2_4 = (gear2.teeth / gear4.teeth)
 
-				correctRatio = ((0.4 <= ratio_1_3 <= 0.6) and (1.4 <= ratio_2_4 <= 1.6))
+                correctRatio = ((minr_1_3 <= ratio_1_3 <= maxr_1_3) and (minr_2_4 <= ratio_2_4 <= maxr_2_4))
 
-				total = gear1.price + gear2.price + gear3.price + gear4.price
+                total = gear1.price + gear2.price + gear3.price + gear4.price
 
-				if correctLength and correctRatio:
-					# print gear1.partNumber, gear2.partNumber, gear3.partNumber, gear4.partNumber, total_s
-					print "Combination found: \n\tTotal cost = $" + str(total)
-					print "\t\tRatio of 1 to 3 = " + str(ratio_1_3)
-					print "\t\t\t(" + str(gear1.teeth) + " : " + str(gear3.teeth) + ")"
-					print "\t\tRatio of 2 to 4 = " + str(ratio_2_4)
-					print "\t\t\t(" + str(gear2.teeth) + " : " + str(gear4.teeth) + ")"
-					print "\t\tAxle-distance   = " + str(length_1_3) + " inches"
-					print "\tPart Numbers:"
-					print "\t\tGear 1 :" + gear1.partNumber
-					print "\t\tGear 2: " + gear2.partNumber
-					print "\t\tGear 3: " + gear3.partNumber
-					print "\t\tGear 4: " + gear4.partNumber
+                if correctDistance and correctRatio:
+                    # print gear1.partNumber, gear2.partNumber, gear3.partNumber, gear4.partNumber, total_s
+                    print "Combination found: \n\tTotal cost = $" + str(total)
+                    print "\t\tRatio of 1 to 3 = " + str(ratio_1_3)
+                    print "\t\t\t(" + str(gear1.teeth) + " : " + str(gear3.teeth) + ")"
+                    print "\t\tRatio of 2 to 4 = " + str(ratio_2_4)
+                    print "\t\t\t(" + str(gear2.teeth) + " : " + str(gear4.teeth) + ")"
+                    print "\t\tAxle-distance   = " + str(dist_1_3) + " inches"
+                    print "\tPart Numbers:"
+                    print "\t\tGear 1 :" + gear1.partNumber
+                    print "\t\tGear 2: " + gear2.partNumber
+                    print "\t\tGear 3: " + gear3.partNumber
+                    print "\t\tGear 4: " + gear4.partNumber
