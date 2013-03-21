6a6c7411ae4adfc4528f6092957014e174375d1c
Matthew Cocco <matthew.cocco@gmail.com>
2013-03-21 10:55:42 -0500

    mixed gearset support - supports pairs of pairs (1 + 3 don't need to be the same pitch as 2 + 4), added new gearset file

--- gearcalc.py
+++ gearcalc.py
@@ -13,6 +13,8 @@ tolerance_2_4   = 0.1
 min_distance = 1.0 # in inches
 max_distance = 2.0 # also in inches
 
+maxPrice = 18.00
+
 # a few calculations before we begin
 
 minr_1_3 = ideal_ratio_1_3 - tolerance_1_3
@@ -36,7 +38,13 @@ class Gear:
         self.partNumber         = cfp.get(gearname, 'PartNumber')
         self.price              = float(cfp.get(gearname, 'Price'))
 
+class Pair:
+    def __init__(self, gear1, gear2):
+        self.ratio = (gear1.teeth / gear2.teeth)
+        self.distance = (gear1.orthogonalDiameter / 2 + gear2.orthogonalDiameter / 2)
+        self.price = (gear1.price + gear2.price)
 
+        self.description = gear1.partNumber + " and " + gear2.partNumber
 
 def evalgears(datafile, num):
 
@@ -77,5 +85,69 @@ def evalgears(datafile, num):
                         print "\t\tGear 3: " + gear3.partNumber
                         print "\t\tGear 4: " + gear4.partNumber
 
-evalgears("gears48_18.ini",18)
-evalgears("gears32_15.ini",15)
+def evalmixedgears(gearSets):
+
+    gears_down = []
+    gears_up   = []
+
+    for gearList in gearSets:
+
+        cfp = ConfigParser.ConfigParser()
+        cfp.readfp(open(gearList))
+
+        numGears = 0
+
+        while True:
+            try:
+                x = Gear(numGears + 1, cfp)
+                numGears = numGears + 1
+            except:
+                break
+
+        gears = [ Gear(i,cfp) for i in range (1, numGears) ]
+
+        # find pairs of gears which satisfy 0.5
+
+        for gear1 in gears:
+            for gear3 in gears:
+                distance = (gear1.orthogonalDiameter / 2 + gear3.orthogonalDiameter / 2)
+                ratio = (gear1.teeth / gear3.teeth)
+
+                correctDistance = (min_distance <= distance <= max_distance)
+                correctRatio = (minr_1_3 <= ratio <= maxr_1_3)
+
+                if correctRatio and correctDistance:
+                    gears_down.append(Pair(gear1, gear3))
+
+        # find pairs of gears which satisfy 1.5
+
+        for gear2 in gears:
+            for gear4 in gears:
+                distance = (gear2.orthogonalDiameter / 2 + gear4.orthogonalDiameter / 2)
+                ratio = (gear2.teeth / gear4.teeth)
+
+                correctDistance = (min_distance <= distance <= max_distance)
+                correctRatio = (minr_2_4 <= ratio <= maxr_2_4)
+
+                if correctRatio and correctDistance:
+                    gears_up.append(Pair(gear2, gear4))
+
+        for pair1 in gears_down:
+            for pair2 in gears_up:
+
+                priceOK = pair1.price + pair2.price <= maxPrice
+                sameDistance = pair1.distance == pair2.distance
+
+                if (priceOK and sameDistance):
+                    print pair1.description + ", " + pair2.description + " - $" + str(pair1.price + pair2.price)
+
+    # find pairs of pairs which are equal in axle-distance
+
+###
+
+# evalgears("gears48_18.ini",18)
+# evalgears("gears32_15.ini",15)
+
+gearsAvailable = ["gears48_18.ini", "gears32_15.ini", "gears_24_4.ini"]
+
+evalmixedgears(gearsAvailable)
